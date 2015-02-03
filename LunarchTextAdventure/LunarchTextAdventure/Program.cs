e77b7d91f943c1dc1067bef9fb86addfa170f513
jordanverasamy <j.verasamy@gmail.com>
2015-02-03 04:12:02 -0500

    core functionality works now

--- LunarchTextAdventure/LunarchTextAdventure/Program.cs
+++ LunarchTextAdventure/LunarchTextAdventure/Program.cs
@@ -8,33 +8,88 @@ namespace LunarchTextAdventure
 {
     class Program
     {
+        static int consoleWidth;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            consoleWidth = 140;
+            Console.SetWindowSize(consoleWidth, 60);
+
+            Dictionary<string, State> stateDict = getStateDict();
+            State state = stateDict["bedroom"];
+
+            string input;
+
+            while (true)
+            {
+                TextWrapper.print("\n" + state.description + " " + state.getStateTransitionData(stateDict), consoleWidth);
+                Console.Write("\n\n>> ", consoleWidth);
+                input = Console.ReadLine();
+                state = getNextState(stateDict, state, input);
+            }
         }
 
-        static Dictionary<string, Location> getStateDict()
+        static State getNextState(Dictionary<string, State> stateDict, State currentState, string action)
         {
-            var n = new Dictionary<string, Location>() {
-                    {"bedroom", new Location(
+            try
+            {
+                return stateDict[currentState.stateTransitions[action]];
+            }
+            catch
+            {
+                Console.WriteLine("\nYour command was not recognized. Here's a list of all valid commands from your current state: \n");
 
-                        "You are in your bedroom. Your monitors emit a soft glow, and the source code of a text-based game is just barely visible from your bed. Extend this eventually.", 
+                for (int i = 0; i < currentState.stateTransitions.Count; i++)
+                {
+                    Console.WriteLine("\"" + currentState.stateTransitions.ElementAt(i).Key + "\"");
+                }
+
+                return currentState;
+            }
+        }
+
+        static Dictionary<string, State> getStateDict()
+        {
+            return new Dictionary<string, State>() {
+                    {"bedroom", new State(
+
+                        "your bedroom", // name of state
+
+                        "You are in your bedroom. Your monitors emit a soft glow, and the source code of a text-based game is just barely visible from your " + 
+                        "bed. You suddenly remember you have an interview with Lunarch Studios today - you should probably head southeast to their office. " + 
+                        "But on the other hand, there's always interesting stuff going on in MC...", // description that is output when in this state
                         
                         new Dictionary<string,string>(){
-                        {"west", "MC Coffee and Donut Shop"},
+                        {"west", "MC CnD"}, // all possible transitions from this state
                         {"east", "Uni/King"}
                         })},
 
-                    {"MC Coffee and Donut Shop", new Location(
+                    {"MC CnD", new State(
 
-                        "You are at MC CnD. Members of the Hearthstone cult sit at tables, paying their tithes to RNJesus and muttering to each other about pay2win. Extend this eventually.", 
+                        "the MC Coffee and Donut Shop",
+
+                        "You are in the MC C&D. Amidst the frantic League of Legends-related clicking, students are working on their assignments and socializing. " + 
+                        "Members of the Hearthstone cult sit at tables, paying their tithes to RNJesus and muttering to each other about pay2win. You're tempted to " + 
+                        "go preach the wonders of RNGless competitive strategy games to them, but that's probably not socially acceptable.",
+
+                        new Dictionary<string,string>(){
+                        {"east", "Uni/King"},
+                        {"preach", "MC CnD-preach"}
+                        })},
+
+                    {"MC CnD-preach", new State(
+
+                        "the MC Coffee and Donut Shop",
+
+                        "You are in the MC C&D. You just went up to a bunch of people playing Hearthstone and started telling them their game sucked. They pretty much " + 
+                        "ignored you, which you feel you should probably have seen coming.",
 
                         new Dictionary<string,string>(){
                         {"east", "Uni/King"}
                         })},
 
-                    {"Uni/King", new Location(
+                    {"Uni/King", new State(
+
+                        "the University/King intersection",
 
                         "You are at University and King. It is so damn cold your fingers might freeze off. A faint, appetizing smell of hot wings extends from the north. Extend this eventually.", 
 
@@ -43,7 +98,9 @@ namespace LunarchTextAdventure
                         {"south", "Starbucks"}
                         })},
 
-                    {"Morty's", new Location(
+                    {"Morty's", new State(
+
+                        "Morty's",
 
                         "You are at Morty's. The delicious smell of hot wings reaches your nostrils. Extend this eventually.", 
 
@@ -51,7 +108,9 @@ namespace LunarchTextAdventure
                         {"south", "Uni/King"}
                         })},
 
-                    {"Starbucks", new Location(
+                    {"Starbucks", new State(
+
+                        "a Starbucks",
 
                         "You are at Starbucks. Girls enjoying their pumpkin spice lattes are abundant, and the beard to male ratio is significantly higher than average. Extend this eventually.", 
 
@@ -60,7 +119,9 @@ namespace LunarchTextAdventure
                         {"south", "LCBO"}
                         })},
 
-                    {"LCBO", new Location(
+                    {"LCBO", new State(
+
+                        "LCBO",
 
                         "You are at LCBO. You're tempted to ditch your interview and just get really drunk. Extend this eventually.", 
 
@@ -69,7 +130,9 @@ namespace LunarchTextAdventure
                         {"south", "Lunarch Office"}
                         })},
 
-                    {"Lunarch Office", new Location(
+                    {"Lunarch Office", new State(
+
+                        "the Lunarch office",
 
                         "You are at the Lunarch office. Alex notices you at the door and waves from his desk. Extend this eventually.", 
 
@@ -78,7 +141,9 @@ namespace LunarchTextAdventure
                         {"west", "interview room"}
                         })},
 
-                    {"interview room", new Location(
+                    {"interview room", new State(
+
+                        "the Lunarch interview room",
 
                         "You are at the Lunarch interview room. Elyot sits down and shakes your hand. At his request, you pull out your laptop and open a text adventure game you made to demo. He smiles and realizes you're incredibly hilarious (and modest to boot). Extend this eventually.", 
 
@@ -86,13 +151,6 @@ namespace LunarchTextAdventure
                         {"leave", "bedroom"}
                         })},
             };
-
-            return n;
-        }
-
-        static Location getNewState()
-        {
-
         }
     }
 }
---
+++ LunarchTextAdventure/LunarchTextAdventure/State.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunarchTextAdventure
+{
+    class State
+    {
+        public string name;
+        public string description;
+        public Dictionary<string, string> stateTransitions;
+
+        public State(string name, string description, Dictionary<string, string> stateTransitions)
+        {
+            this.name = name;
+            this.description = description;
+            this.stateTransitions = stateTransitions;
+        }
+
+        public string getStateTransitionData(Dictionary<string, State> stateDict)
+        {
+            Random rnd = new Random();
+            int n;
+            int numChoices = 3;
+            string output = "";
+
+            for (int i = 0; i < this.stateTransitions.Count; i++)
+            {
+                var currentElement = this.stateTransitions.ElementAt(i);
+
+                if (currentElement.Key == "west" ||
+                    currentElement.Key == "east" ||
+                    currentElement.Key == "north" ||
+                    currentElement.Key == "south")
+                {
+                    n = rnd.Next(numChoices);
+
+                    if (n == 1)
+                        output = output + "To the " + currentElement.Key + " lies " + stateDict[currentElement.Value].name + ". ";
+                    else if (n == 2)
+                        output = output + "To your " + currentElement.Key + ", there's " + stateDict[currentElement.Value].name + ". ";
+                    else
+                        output = output + TextWrapper.UppercaseFirst(stateDict[currentElement.Value].name) + " is to the " + currentElement.Key + ". ";
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return output;
+
+        }
+        
+
+    }
+}
